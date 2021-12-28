0177d25e4e64b989098316623caeab1937f787b4
shani-arnon <sarnon@datto.com>
2021-12-28 09:56:34 +0200

    -

--- server/index.js
+++ server/index.js
@@ -5,7 +5,6 @@ import fs from 'fs/promises';
 const app = express();
 app.use(express.json());
 app.use(cors());
-
 app.get('/api/latest',async (req, res) => {
     const content = JSON.parse(await fs.readFile('./data/meta.json'))
     res.status(200).json(content)
