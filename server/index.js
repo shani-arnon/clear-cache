d3b6e71ff2bc936c5465be91ba9fd600e1eb8abb
shani-arnon <sarnon@datto.com>
2021-12-28 10:49:04 +0200

    start with socket.io

--- server/index.js
+++ server/index.js
@@ -1,14 +1,25 @@
-import express from "express";
-import cors from "cors";
-import fs from 'fs/promises';
+// # region BK
+// import express from "express";
+// import cors from "cors";
+// import fs from 'fs/promises';
 
-const app = express();
-app.use(express.json());
-app.use(cors());
-app.get('/api/latest',async (req, res) => {
-    const content = JSON.parse(await fs.readFile('./data/meta.json'))
-    res.status(200).json(content)
+// const app = express();
+// app.use(express.json());
+// app.use(cors());
+// app.get('/api/latest',async (req, res) => {
+//     const content = JSON.parse(await fs.readFile('./data/meta.json'))
+//     res.status(200).json(content)
+// });
+
+
+// app.listen(3030, () => console.log('server is up!'));
+
+const app = require("express")();
+const httpServer = require("http").createServer(app);
+const options = {};
+const io = require("socket.io")(httpServer, options);
+
+io.on("connection", socket => {
+    console.log('connection established', { socket });
 });
-
-
-app.listen(3030, () => console.log('server is up!'));
+httpServer.listen(3030);
